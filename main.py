fa3c498911c4f4200d09263e401cea97332c0ee0
nekstas <nek.stas.other@yandex.ru>
2023-02-16 02:09:00 +0300

    Изменена главная страничка с тестовой на более работающую

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Автор: Некрасов Станислав
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def index_page():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
